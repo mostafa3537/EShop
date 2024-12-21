53357d36e09baf232aa3b037a382cd442d481f16
mostafa3537 <Mostafa.elsayed3537@gmail.com>
2024-12-21 22:20:45 +0200

    grpc service

--- Services/Discount/Discount.Grpc/Program.cs
+++ Services/Discount/Discount.Grpc/Program.cs
@@ -1,15 +1,28 @@
-namespace Discount.Grpc
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+        var builder = WebApplication.CreateBuilder(args);
 
-            app.MapGet("/", () => "Hello World!");
+        // Add services to the container.
+        builder.Services.AddGrpc();
+        builder.Services.AddDbContext<DiscountContext>(opts =>
+                opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
-            app.Run();
-        }
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseMigration();
+        app.MapGrpcService<DiscountService>();
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+        app.Run();
+
     }
 }
