4d87fa0705398ba4e27a000c15b23dad1f3b68b8
mostafa3537 <Mostafa.elsayed3537@gmail.com>
2025-01-01 03:09:23 +0200

    add services injection

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,15 +1,29 @@
-namespace Ordering.API
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+namespace Ordering.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services
+            .AddApplicationServices(builder.Configuration)
+            .AddInfrastructureServices(builder.Configuration)
+            .AddApiServices(builder.Configuration);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseApiServices();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
+            //await app.InitialiseDatabaseAsync();
         }
+
+        app.Run();
     }
 }
